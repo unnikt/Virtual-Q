982b19064d4902be82d8b1ce28533936d0566c09
unnikt <unnikt@gmail.com>
2020-05-10 09:03:37 +1000

    mainNav and Settings page changes

--- public/js/signin.js
+++ public/js/signin.js
@@ -3,13 +3,15 @@ var bLoggedIn = false;
 
 auth.onAuthStateChanged(user=>{
     if (page==='signup') window.location='index';
-    if(user)    {
+    if (user) {
+        document.getElementById('mainNav').style.width = '100%'; 
         document.getElementById("signinform").style.display="none";
-        document.getElementById("gridicons").style.display="grid";
+        // document.getElementById("gridicons").style.display="grid";
     }
     else
     {   document.getElementById("signinform").style.display="grid";
-        document.getElementById("gridicons").style.display="none";
+        document.getElementById('mainNav').style.width = '0%'; 
+        // document.getElementById("gridicons").style.display="none";
         document.getElementById("errMsg").innerText="Please sign in..";
     }
     // document.getElementById("banner").style.display='none';
