4195ebb467f65838da7a1873290d4d6478fb900c
unnikt <unnikt@gmail.com>
2020-05-11 11:43:22 +1000

    Implemented Firebase UI

--- public/js/signin.js
+++ public/js/signin.js
@@ -5,14 +5,14 @@ auth.onAuthStateChanged(user=>{
     if (page==='signup') window.location='index';
     if (user) {
         document.getElementById('mainNav').style.width = '100%'; 
-        document.getElementById("signinform").style.display="none";
+        document.getElementById("firebaseui-auth-container").style.display="none";
         // document.getElementById("gridicons").style.display="grid";
     }
-    else
-    {   document.getElementById("signinform").style.display="grid";
+    else{
+    // {   document.getElementById("signinform").style.display="grid";
         document.getElementById('mainNav').style.width = '0%'; 
-        // document.getElementById("gridicons").style.display="none";
-        document.getElementById("errMsg").innerText="Please sign in..";
+        document.getElementById("firebaseui-auth-container").style.display="block";
+        // document.getElementById("errMsg").innerText="Please sign in..";
     }
     // document.getElementById("banner").style.display='none';
 })
