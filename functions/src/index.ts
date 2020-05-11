e1510ba3c281287f85d53ebc1ce430661f7d99e7
unnikt <unnikt@gmail.com>
2020-05-11 17:41:59 +1000

    Changes settings for service provider accounts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
 
+import * as main from './main';
+import * as settings from './settings';
 import * as services from './services';
 import * as skedules from './skedules';
-import * as main from './main';
 import * as store from './dbstore';
-import * as settings from './settings';
     
 module.exports = {
     ...main,
--- functions/src/settings.ts
+++ functions/src/settings.ts
@@ -22,21 +22,20 @@ settings.use(express.json({ limit: '1mb' })); //set up to receive data in JSON f
 
 // This function sign's out the user 
 settings.get('/settings', (request, response) => cors(request, response, () => {
-
     const uid = "GUV33P1scHeRqbnZF0t4RRjsJ5K2";
-    const params = { provider: false, orgname: "" }
+    const params:{ orgs: { id: String, orgname: String }[]} = {orgs:[]};
 
     // Get the provider doc for the user
-    db.collection("providers").doc(uid).get()
-        .then(doc => {
-            if (doc.exists) {
-                params.provider = true;
-                params.orgname = doc.get("orgname");
-            } 
+    const providers = db.collection("providers");
+    providers.where('uid', '==', uid).get()
+        .then(querySanpshot => {
+            querySanpshot.forEach(doc => {
+                params.orgs.push({ id: doc.id, orgname: doc.get("orgname") })
+            });
+            console.log(params);
+            response.render("settings", params);
         })
-    .catch (err=>console.log(err))
-
-    response.render("settings", params);
+        .catch(err => response.send("Error - " + err))
 }));
 
 exports.settings = functions.https.onRequest(settings);
